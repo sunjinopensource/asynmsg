457979261026c25de341641c38ee2d1836c6d522
sunjin <sunjinopensource@qq.com>
2015-07-02 17:56:07 +0800

    examples/protobuf handle msg_data/msg_cls None

--- examples/protobuf/protobuftools.py
+++ examples/protobuf/protobuftools.py
@@ -1,12 +1,15 @@
 import asynmsg
 import struct
 
-def protobuf_handler_config(msg_id, msg_cls):
+def protobuf_handler_config(msg_id, msg_cls=None):
     def wrapper(func):
         @asynmsg.message_handler_config(msg_id)
         def wrapper2(self, msgid, msg_data):
-            proto_data = msg_cls()
-            proto_data.ParseFromString(msg_data)
+            if msg_cls is None:
+                proto_data = msg_data
+            else:
+                proto_data = msg_cls()
+                proto_data.ParseFromString(msg_data)
             return func(self, msgid, proto_data)
         return wrapper2
     return wrapper
@@ -15,7 +18,8 @@ def protobuf_handler_config(msg_id, msg_cls):
 class SessionS(asynmsg.SessionS):
     def encode_message_to_bytes(self, msg_id, msg_data):
         bytes = struct.pack('H', msg_id)
-        bytes += msg_data.SerializeToString()
+        if msg_data is not None:
+            bytes += msg_data.SerializeToString()
         return bytes
 
     def decode_message_from_bytes(self, bytes):
@@ -26,7 +30,8 @@ class SessionS(asynmsg.SessionS):
 class SessionC(asynmsg.SessionC):
     def encode_message_to_bytes(self, msg_id, msg_data):
         bytes = struct.pack('H', msg_id)
-        bytes += msg_data.SerializeToString()
+        if msg_data is not None:
+            bytes += msg_data.SerializeToString()
         return bytes
 
     def decode_message_from_bytes(self, bytes):
